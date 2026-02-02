3fbd0b3d0315dc3e37e73917889408beae8aac6c
Julian Isaza <julian.isaza1020@gmail.com>
2026-02-02 18:10:07 -0500

    actualización de dependencias y selenium 5

--- src/test/java/enlace/screenplay/CucumberTestSuite.java
+++ src/test/java/enlace/screenplay/CucumberTestSuite.java
@@ -1,18 +1,30 @@
 package enlace.screenplay;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-/**
- * Runner principal para ejecutar las pruebas con Cucumber y Serenity
- */
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-    features = "classpath:features",
-    glue = "enlace.screenplay.stepdefinitions",
-    plugin = {"pretty", "json:target/cucumber.json"},
-    tags = "@test"
+
+import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "enlace.screenplay.stepdefinitions"
+)
+@ConfigurationParameter(
+        key = FILTER_TAGS_PROPERTY_NAME,
+        value = "@test"
+)
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, net.serenitybdd.cucumber.core.plugin.SerenityReporter"
 )
 public class CucumberTestSuite {
 }
